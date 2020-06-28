2c91b7e854ca02b68130d40e97fd96fedf69929f
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-06-28 13:29:44 +0530

    removed queueBroker.js with redis

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const redis = require('redis');
 const client = redis.createClient({ db: 1 });
-const http = require('http');
 const imageSets = require('./imageSets');
 
 const PORT = 8000;
@@ -11,10 +10,6 @@ app.use((req, res, next) => {
   console.log(`${req.method}, ${req.url}`);
   next();
 });
-
-const getQueueBrokerOptions = function () {
-  return { host: 'localhost', port: 8001, method: 'post', path: '/push-job/' };
-};
 
 app.get('/status/:id', (req, res) => {
   imageSets.get(client, req.params.id).then((imageSet) => {
@@ -26,11 +21,7 @@ app.post('/process/:name/:count/:width/:height/:tags', (req, res) => {
   imageSets.addImageSet(client, req.params).then((id) => {
     res.send(`id:${id}`);
     res.end();
-    const options = getQueueBrokerOptions();
-    options.path = options.path + id;
-    const req = http.request(options, () => {});
-    // jobs.push({ id });
-    req.end();
+    client.lpush('imageList', id, () => {});
   });
 });
 
--- src/worker.js
+++ src/worker.js
@@ -1,28 +1,17 @@
-const http = require('http');
 const redis = require('redis');
 const client = redis.createClient({ db: 1 });
 const imageSets = require('./imageSets');
 const processImage = require('./processImage');
 
-const getWorkerOptions = function () {
-  return { host: 'localhost', port: 8001, method: 'get', path: '/request-job' };
-};
-
 const getJob = function () {
   return new Promise((resolve, reject) => {
-    const options = getWorkerOptions();
-    const req = http.request(options, (res) => {
-      let data = '';
-      res.on('data', (chunk) => (data += chunk));
-      res.on('end', () => {
-        if (JSON.parse(data).id != undefined) {
-          resolve(JSON.parse(data).id);
-        } else {
-          reject('No job found');
-        }
-      });
+    client.brpop('imageList', 1, (err, res) => {
+      if (res) {
+        resolve(res[1]);
+      } else {
+        reject('no job');
+      }
     });
-    req.end();
   });
 };
 
@@ -40,7 +29,7 @@ const runLoop = function () {
           .then(runLoop);
       });
     })
-    .catch(() => setTimeout(runLoop, 1000));
+    .catch(runLoop);
 };
 
 runLoop();
